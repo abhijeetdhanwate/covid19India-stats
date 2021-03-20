3441ecac4f2a0510e2f9f0b1e329e0d19cccbb63
coderAbhii <62416129+coderAbhii@users.noreply.github.com>
2021-03-20 10:37:09 +0530

    More responsive title and text formatting.

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-// import axios from 'axios'
-
 const url = "https://api.covid19india.org/data.json";
 axios.get(url)
 .then(data=>{
-    document.getElementById("confirmed").innerHTML = data["data"]["statewise"][0]["confirmed"];
-    document.getElementById("active").innerHTML = data["data"]["statewise"][0]["active"];
-    document.getElementById("recovered").innerHTML = data["data"]["statewise"][0]["recovered"];
-    document.getElementById("deaths").innerHTML = data["data"]["statewise"][0]["deaths"];
+    document.getElementById("confirmed").innerHTML = data["data"]["statewise"][0]["confirmed"].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    document.getElementById("active").innerHTML = data["data"]["statewise"][0]["active"].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    document.getElementById("recovered").innerHTML = data["data"]["statewise"][0]["recovered"].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    document.getElementById("deaths").innerHTML = data["data"]["statewise"][0]["deaths"].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 
     for (let i=0; i<38; i++){
         statesContainer = document.getElementsByClassName('states')[0];
@@ -30,19 +28,16 @@ axios.get(url)
         state.appendChild(active);
         state.appendChild(recovered);
         state.appendChild(deaths);
-        
-            // document.getElementsByClassName('stateName')[i].innerHTML = "State";                                                        
-            // document.getElementsByClassName('confirmed')[i].innerHTML = "Confirmed";
-            // document.getElementsByClassName('active')[i].innerHTML = "Active";
-            // document.getElementsByClassName('recovered')[i].innerHTML = "Recovered";
-            // document.getElementsByClassName('deaths')[i].innerHTML = "Deaths";
 
         document.getElementsByClassName('stateName')[i].innerHTML = data["data"]["statewise"][i]["state"];                                                        
-        document.getElementsByClassName('confirmed')[i].innerHTML = data["data"]["statewise"][i]["confirmed"].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');;                                                        
-        document.getElementsByClassName('active')[i].innerHTML = data["data"]["statewise"][i]["active"].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');;
-        document.getElementsByClassName('recovered')[i].innerHTML = data["data"]["statewise"][i]["recovered"].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');;
-        document.getElementsByClassName('deaths')[i].innerHTML = data["data"]["statewise"][i]["deaths"].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');;
+        document.getElementsByClassName('confirmed')[i].innerHTML = data["data"]["statewise"][i]["confirmed"].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');                                                        
+        document.getElementsByClassName('active')[i].innerHTML = data["data"]["statewise"][i]["active"].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+        document.getElementsByClassName('recovered')[i].innerHTML = data["data"]["statewise"][i]["recovered"].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+        document.getElementsByClassName('deaths')[i].innerHTML = data["data"]["statewise"][i]["deaths"].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
     }
 })
 .catch(err=>console.log(err));
 
+let navHeight = document.getElementsByTagName("nav")[0].offsetHeight;
+document.querySelector(".state-head").style.top = String(parseInt(navHeight) + 1 + "px");
+document.querySelector("#credit").style.lineHeight = String(parseInt(navHeight) + "px")
